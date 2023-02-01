23d7aa75507c3b3a6aee783fbc30eda82689a841
marik <gaurav.marik@gmail.com>
2023-02-01 00:44:09 -0500

    router-link, route.js, ListAppointment, ListUsers, UpdateSettings

--- resources/js/pages/users/ListUsers.vue
+++ resources/js/pages/users/ListUsers.vue
@@ -129,12 +129,12 @@ onMounted(() => {
                 <div class="col-sm-6">
                     <h1 class="m-0">Users</h1>
                 </div>
-                <div class="col-sm-6">
+                <!-- <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
                         <li class="breadcrumb-item active">Appointments</li>
                     </ol>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
