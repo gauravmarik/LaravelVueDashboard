eafffef552b65219b51f108398bd1586103c7de5
marik <gaurav.marik@gmail.com>
2023-02-01 22:55:24 -0500

    app.blade.php, router-view

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -230,24 +230,12 @@
 
             <div class="content-header">
 
-                <!-- <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">Starter Page</h1>
-                        </div>
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">Starter Page</li>
-                            </ol>
-                        </div>
-                    </div>
-                </div> -->
+               
             </div>
 
 
             <div class="content">
-                <!-- <div class="container-fluid">
+                <div class="container-fluid">
                     <div class="row">
                         <div class="col-lg-6">
                             <div class="card">
@@ -297,7 +285,7 @@
 
                     </div>
 
-                </div> -->
+                </div>
             </div>
 
         </div> -->
